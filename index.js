403d415b9964333584c8242985383b2ad2b6fdda
bahayg <bahayg@gmail.com>
2020-01-15 15:55:27 -0800

    Done.

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const findMatching = (drivers, name) => {
 }
 
 const fuzzyMatch = (drivers, string) => {
-    return drivers.filter(driver => driver[0] === string[0])
+    // return drivers.filter(driver => driver[0] === string[0])
+    return drivers.filter(driver => driver.slice(0,string.length) == string)
 }
 
 const matchName = (drivers, name) => {
