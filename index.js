63c15e10a52c17a017e019cde4ffe1fdc775f9fa
4myc <missch3n@gmail.com>
2020-01-15 14:22:25 -0800

    Done.

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
 // returns all drivers that match the passed in name
 const findMatching = (drivers, name) => {
     // return drivers.filter(drivers => drivers.toLowerCase() == name.toLowerCase())
-    return drivers.filter(drivers => drivers.toUpperCase() === name.toUpperCase())
+    return drivers.filter(driver => driver.toUpperCase() === name.toUpperCase())
     // return drivers.filter(drivers => drivers[0].toUpperCase() === name[0].toUpperCase())
 }
 
 // returns a driver if beginning provided letters match
 const fuzzyMatch = (drivers, name) => {
-    return drivers.filter(drivers => drivers[0].toUpperCase() == name[0].toUpperCase())
+    return drivers.filter(driver => driver[0].toUpperCase() == name[0].toUpperCase())
 }
 
 // accesses the data structure to check if name matches
 const matchName = (drivers, name) => {
-    return drivers.filter(drivers => drivers.name === name)
+    return drivers.filter(driver => driver.name === name)
 }
